805714cf5ed7bb23817ae0a2e39935f5b3c7ec7e
Max Marchuk <maxmarchuk@gmail.com>
2014-10-31 20:14:40 -0700

    actually added the controllers this time

---
+++ app/scripts/controllers/create.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name secretSantaApp.controller:CreateCtrl
+ * @description
+ * # CreateCtrl
+ * Controller of the secretSantaApp
+ */
+angular.module('secretSantaApp')
+  .controller('CreateCtrl', ['$scope', 'dbService', function ($scope, dbService) {
+    $scope.created = false;
+    $scope.header = 'New Group';
+    $scope.submit = function() {
+        console.log('asfasf');
+        if ($scope.name && $scope.email) {
+            $scope.created = true;
+            $scope.header = 'Group Created!';
+            dbService.makeGroup($scope.email, $scope.name);
+        }
+    };
+  }]);
---
+++ app/scripts/services/dbservice.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name secretSantaApp.dbService
+ * @description
+ * # dbService
+ * Service to manage any database transactions
+ */
+angular.module('secretSantaApp')
+    .service('dbService', function dbService() {
+        return {
+
+            makeGroup : function (email, name) {
+                return {
+                    editURL: '/edit/12345',
+                    shareURL: '/join/67890'
+                };
+            joinGroup : 
+            }
+        };
+    });
