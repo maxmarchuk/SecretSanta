819c0f87a9346e003116de06ded3f526fde7111b
Chance Snow <chances@cat.pdx.edu>
2014-10-31 22:27:52 -0700

    Add more groupies

--- app/scripts/services/dbservice.js
+++ app/scripts/services/dbservice.js
@@ -8,24 +8,25 @@
  * Service to manage any database transactions
  */
 angular.module('secretSantaApp')
-    .service('dbService', function dbService() {
+  .service('dbService', function dbService() {
+    return {
+
+      makeGroup: function (email, name) {
         return {
-
-            makeGroup: function (email, name) {
-                return {
-                    editURL: '/edit/12345',
-                    shareURL: '/join/67890'
-                };
-            },
-            joinGroup: function (email, name, hash) {
-
-                return true;
-
-            },
-            getGroupies: function (hash) {
-                //hard coded value of people in group for now
-                //TODO: get groupies from DB, man.
-                return ['Samantha Wallace', 'Victor Borishevkin', 'Lamar Hawk', 'Max Marchuk', 'Casey Marchuk', 'Rebecca Stone', 'Chance Snow', 'Steven Snow'];
-            }
+          editURL: '/edit/12345',
+          shareURL: '/join/67890'
         };
-    });
+      },
+      joinGroup: function (email, name, hash) {
+
+        return true;
+
+      },
+      getGroupies: function (hash) {
+        //hard coded value of people in group for now
+        //TODO: get groupies from DB, man.
+        return ['Samantha Wallace', 'Victor Borishevkin', 'Lamar Hawk', 'Max Marchuk', 'Casey Marchuk',
+          'Rebecca Stone', 'Chance Snow', 'Joe Snow', 'Jack Johnson', 'Alden Sparks'];
+      }
+    };
+  });
